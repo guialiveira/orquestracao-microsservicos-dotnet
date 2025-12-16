25df90e5cb05104324e510757ce08da1ab46e007
dpcosta <dpcosta@gmail.com>
2025-12-16 06:29:20 -0300

    Mudanças no vídeo.

--- VollMed.Gateway/Program.cs
+++ VollMed.Gateway/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,33 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var keycloakConfig = builder.Configuration.GetSection("Authentication:KeyCloak");
+
+        options.Authority = keycloakConfig["Authority"];
+        options.Audience = keycloakConfig["Audience"];
+        options.RequireHttpsMetadata = keycloakConfig.GetValue<bool>("RequireHttpsMetadata");
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = keycloakConfig.GetValue<bool>("ValidateAudience"),
+            ValidateIssuer = keycloakConfig.GetValue<bool>("ValidateIssuer"),
+            ValidateLifetime = keycloakConfig.GetValue<bool>("ValidateLifetime"),
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+builder.Services
+    .AddAuthorization(options =>
+    {
+        options.AddPolicy(
+            "authenticated", policy => policy.RequireAuthenticatedUser()
+        );
+    });
 
 var app = builder.Build();
 
@@ -16,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
